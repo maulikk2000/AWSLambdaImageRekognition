05f19fd1d6d421a2a4c769880e83a4e05e07ce5a
maulikk2000 <maulikk2000@gmail.com>
2018-08-23 23:04:07 +1000

    init commit

---
+++ ImageOrNotClient/ImageDetector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Lambda;
+using Amazon.Lambda.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace ImageOrNotClient
+{
+    public class ImageDetector
+    {
+        private readonly string _accessKey;
+        private readonly string _secretKey;
+
+        public ImageDetector(string accessKey, string secretKey)
+        {
+            _accessKey = accessKey;
+            _secretKey = secretKey;
+        }
+
+        private async Task<Boolean> IsImage(string filePath, string fileName)
+        {
+            await UploadToS3(filePath, fileName);
+
+            var amazonLambdaClient = new AmazonLambdaClient(_accessKey, _secretKey, Amazon.RegionEndpoint.APSouth1);
+
+            InvokeRequest ir = new InvokeRequest
+            {
+                InvocationType = InvocationType.RequestResponse,
+                FunctionName = "ImageOrNot", //name of the function which we created when we deployed the func.
+                Payload = "\"" + fileName + "\""
+            };
+
+            var result = await amazonLambdaClient.InvokeAsync(ir);
+
+            var strResponse = Encoding.ASCII.GetString(result.Payload.ToArray());
+
+            if(bool.TryParse(strResponse, out bool retval))
+            {
+                return retval;
+            }
+
+            return false;
+        }
+
+        private async Task UploadToS3(string filePath, string fileName)
+        {
+            var client = new AmazonS3Client(_accessKey, _secretKey, Amazon.RegionEndpoint.APSouth1);
+
+            var putRequest = new PutObjectRequest
+            {
+                BucketName = "somebucket",
+                Key = fileName,
+                FilePath = filePath,
+                ContentType = "text/plain"
+            };
+
+            var response = await client.PutObjectAsync(putRequest);
+        }
+    }
+}
--- ObjectImageOrNot/Function.cs
+++ ObjectImageOrNot/Function.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -14,14 +16,41 @@ namespace ObjectImageOrNot
     {
         
         /// <summary>
-        /// A simple function that takes a string and does a ToUpper
+        /// A simple function that takes an image name as an input and returns whether it's abc or not?
         /// </summary>
-        /// <param name="input"></param>
+        /// <param name="input">name of the image we want to detect</param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public async Task<bool> FunctionHandler(string input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            var rekognitionClient = new AmazonRekognitionClient();
+
+            var response = await rekognitionClient.DetectLabelsAsync(
+                new DetectLabelsRequest
+                {
+                    Image = new Image
+                    {
+                        S3Object = new S3Object
+                        {
+                            Bucket = "somebucket",
+                            Name = input //the name of the image we are detecting
+                        }
+                    }
+                });
+
+            //iterate through all the labels for the real-world object detected
+            foreach (var label in response.Labels)
+            {
+                if(label.Confidence > 50) //here if confidence is more than 50, we make the call that it's the image we are looking for
+                {
+                    if(label.Name == "Fried Chicken" || label.Name == "Nuggets")
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
         }
     }
 }
